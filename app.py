81f36b30f6aae44a453aadf10e6abeac9b6f712f
Chi-Shan Yu <chisamyu@seed.net.tw>
2021-11-19 08:05:40 +0800

    jwt server

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 import uvicorn
 from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return "Hello World for Root Page"
+from fastapi.middleware.cors import CORSMiddleware
+from router import product, user, authentication
+from db import models
+from db.database import engine
 
 
-@app.get("/products")
-def products():
-    return "THIS IS A PRODUCTS PAGE"
+app = FastAPI(
+    title="Shopping Cart API",
+    description="This API was developed for teaching Fast API",
+    version="0.0.1",
+    terms_of_service="http://localhost:5000",
+)
 
-def get_product():
-    ...
+app.include_router(authentication.router)
+app.include_router(product.router)
+app.include_router(user.router)
+
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", port=5000, reload=True)
+    uvicorn.run("app:app", port= 5000, reload=True)
+
+
+origins = [
+    'http://localhost:3000',
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=['*']
+)
+
+models.Base.metadata.create_all(engine)
